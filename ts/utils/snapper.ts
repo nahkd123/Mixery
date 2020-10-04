db7b0d4afbea28188d6203ea3a312a877c8248ae
nahkd123 <nahkd123@gmail.com>
2020-10-04 13:54:45 +0700

    Add fixed snap function

--- ts/utils/snapper.ts
+++ ts/utils/snapper.ts
@@ -7,5 +7,8 @@ export function snap(val: number, ...vas: number[]) {
     });
     return hval + sel;
 }
+export function fixedSnap(val: number, segment: number) {
+    return Math.floor(val / segment) * segment;
+}
 
 export const BeatSnapPreset = [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1];
