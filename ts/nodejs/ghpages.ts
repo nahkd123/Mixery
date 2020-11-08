f444b098512f05d407e1f830cb41e5245fc3c7b3
nahkd123 <nahkd123@gmail.com>
2020-11-08 14:01:32 +0700

    ➕ Add GitHub pages auto commit thing ayy

--- ts/nodejs/ghpages.ts
+++ ts/nodejs/ghpages.ts
@@ -4,3 +4,35 @@ origin remote
 */
 
 import * as ghpages from "gh-pages";
+
+interface GHPagesOptions {
+    src?: string | string[];
+
+    /** @default "gh-pages" */
+    branch?: string;
+
+    /** @default "." */
+    dest?: string;
+
+    /** @default false */
+    dotfiles?: boolean;
+    
+    /** @default false */
+    add?: boolean;
+    
+    /** @default origin remote URL */
+    repo?: boolean;
+    
+    /** @default "origin" */
+    remote?: string;
+
+    /** @default "Update" */
+    message?: string;
+}
+
+ghpages.publish("./temp/production", <GHPagesOptions> {
+    remote: "origin",
+    message: "🦴 Auto-generated commit (ghpages.ts)"
+}, err => {
+    if (err) console.error(err);
+});
