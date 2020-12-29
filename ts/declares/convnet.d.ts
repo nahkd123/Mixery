a68533d9b86ec6a29324acead3f020416fb1a632
nahkd123 <nahkd123@gmail.com>
2020-12-29 19:16:46 +0700

    🦴 Prepare ConvNetJS

---
+++ ts/declares/convnet.d.ts
@@ -0,0 +1,71 @@
+declare namespace convnetjs {
+    export const REVISION: string;
+
+    export interface LayerDefinition {
+        type: "input" | "fc" | "softmax" | "svm" | "regression";
+
+        out_sx?: number;
+        out_sy?: number;
+        out_depth?: number;
+
+        num_neurons?: number;
+        activation?: "sigmoid" | "tanh" | "relu" | "maxout";
+
+        num_classes?: number;
+    }
+    export class Net {
+        makeLayers(
+            layerDefs: LayerDefinition[]
+        );
+        toJSON(): object;
+        fromJSON(obj: object);
+
+        forward(
+            vol: Vol
+        ): Vol;
+    }
+
+    export interface TrainerOptions {
+        method: "adadelta" | "adagrad" | "sgd";
+        
+        learning_rate?: number;
+        l1_decay?: number;
+        l2_decay?: number;
+        momentum?: number;
+        batch_size?: number;
+    }
+    export class Trainer {
+        constructor(
+            net: Net,
+            options: TrainerOptions
+        );
+        train(vol: Vol, clss: number);
+        train(vol: Vol, nr: number[]);
+    }
+    export class Vol {
+        constructor(
+            x: number,
+            y: number,
+            z: number,
+            def?: number
+        );
+        constructor(
+            def: number[]
+        );
+
+        w: number[];
+        dw: number[];
+
+        set(
+            x: number,
+            y: number,
+            z: number,
+            value: number
+        );
+        get(
+            x: number,
+            y: number,
+            z: number
+        );
+    }
+}
